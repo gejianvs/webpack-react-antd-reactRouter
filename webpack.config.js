97be5ca67c3910e92df7650fddaa1c2063ec92fc
gejian <80782982@qq.com>
2021-03-16 21:17:29 +0800

    测试git 提交

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        enforce: 'pre',
+        use: [{
+            loader: 'babel-loader',
+        }, {
+            loader: 'eslint-loader', // 指定启用eslint-loader
+            options: {
+                formatter: require('eslint-friendly-formatter'),
+                emitWarning: false
+            }
+        }]
+      },
+      {
+        test: /\.(css|less)$/,
+        exclude: /node_modules/,
+        include: /src/,
+        use: [
+          {loader: "style-loader"},
+          {
+              loader: 'css-loader',
+              options: {
+                  minimize: process.env.NODE_ENV === 'production',
+                  importLoaders: 2,
+                  localIdentName: '[name]-[local]-[hash:base64:5]',
+                  modules:true
+              }
+          }, {
+              loader: 'postcss-loader',
+              options: {           // 如果没有options这个选项将会报错 No PostCSS Config found
+                  plugins: (loader) => [
+                      require('autoprefixer')(), //CSS浏览器兼容
+                  ]
+              }
+          },{
+              loader: 'less-loader',
+              options: {
+                  javascriptEnabled: true,
+              }
+          }],
+      }
+    ]
+  },
+  mode: 'development'
+};
