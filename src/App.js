b11426562ab046ea2c0ddc3a8b9798d2c7622c25
gejian <80782982@qq.com>
2021-03-20 20:13:26 +0800

    tes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
-class App extends Component{
-  render(){
-    return(
-      <div>1111111</div>
-    )
-  }
+import { Router, Route, Link, browserHistory } from 'react-router'
+
+
+ function App(){
+  return (
+    <div>
+      <h1>App</h1>
+      <ul>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/inbox">Inbox</Link></li>
+      </ul>
+      {this.props.children}
+    </div>
+  )
 }
 
 export default App;
