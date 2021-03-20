35c0b5c50e5f7b812216fbf0c397451de70a2e73
gejian <80782982@qq.com>
2021-03-20 20:17:28 +0800

    tes

--- src/App.js
+++ src/App.js
@@ -6,11 +6,6 @@ import { Router, Route, Link, browserHistory } from 'react-router'
   return (
     <div>
       <h1>App</h1>
-      <ul>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/inbox">Inbox</Link></li>
-      </ul>
-      {this.props.children}
     </div>
   )
 }
